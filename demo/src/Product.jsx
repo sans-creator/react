4d7219f05153ae092144edc229a3921394791d79
sans-creator <sanskarj163@gmail.com>
2025-07-11 23:45:56 +0530

    passingArray

--- demo/src/Product.jsx
+++ demo/src/Product.jsx
@@ -1,11 +1,18 @@
 import "./Product.css";
 
-function Product({ title, price =1}) {  // Destructure the props to get title directly
+function Product({ title, price,feature,fe2}) {  // Destructure the props to get title directly
   return (
     <div className="Product">
       <h2> {title}</h2>
       <p>This is a product of worth {price}.</p>
+      <p> 
+        {feature}
+      </p>
+        <p>
+            {fe2?.a}
+        </p>
     </div>
+
   );
 }
 
--- demo/src/ProductTab.jsx
+++ demo/src/ProductTab.jsx
@@ -1,10 +1,11 @@
 import Product from "./Product";
 
 function ProductTab() {
+  let features = ["phone", "pen", "tab"];
   return (
     <>
-    <Product title="phone" price={ 10} />
-  <Product  title="pen" />
+    <Product title="phone" price={10} feature={features} fe2={{a: "hi-tech", b: "expensive"}} />
+    <Product title="pen" price={1} />
     <Product title="tab" price={222} />
     </>
    
